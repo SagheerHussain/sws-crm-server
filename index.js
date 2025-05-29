c0c0e47c749646ee3fafc64f272009d73069fc1b
muhammad-ahsan-kz <muhammad.ahsan@skynetsilicon.com>
2025-05-29 17:44:05 +0500

    parsed credentials json file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // Initialize Google Drive API Auth
 const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_CREDENTIALS,
+    keyFile: JSON.parse(process.env.GOOGLE_CREDENTIALS),
     scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
